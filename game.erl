0b4f1915fe8cfa8202acd9b2ebf0df2475767fb5
Kruchy <kruchy61@gmail.com>
2015-01-10 14:49:54 +0100

    working

--- game.erl
+++ game.erl
@@ -6,7 +6,7 @@
 %-import(view,start).
 -record(board,{area,width,height}).
 
-checkXY(W,H,X,Y) when (X >0 ) and (X < W) and (Y < H) and (Y >0)-> true;
+checkXY(W,H,X,Y) when (X >0 ) and (X < W-1) and (Y < H-1) and (Y >0)-> true;
 checkXY(_,_,_,_) -> false.
 
 getA(Board,X,Y) -> array:get(Y,array:get(X,Board#board.area)).
@@ -23,29 +23,34 @@ set( X, Y, Value, Array ) ->
 	array:set( X, New_y_array, Array ).
 
 
-
-
+size(Board) -> io:fwrite("~p ~p ~n",[Board#board.height,Board#board.width]).
+seq(Board) -> lists:seq(1,Board#board.width-2).
 startGame(W,H)->
 	Board = createBoard(W,H),
 	initBoard(Board),
-	view:start(W,H),
-	spawn(?MODULE,next,[Board,W,H]).
-
-
-next(Board,W,H)->
-	receive 
-		after 1000 -> nextIteration(Board,W,H)
+	Frame = view:initFrame(W,H),
+	case (lists:member(frame,registered())) of
+		true -> ok;
+		false -> register(frame,spawn(view,loop,[Frame,W,H,0]))
 	end,
-	next(Board,W,H).
 
-nextIteration(Board,Height,Width) ->
-	[ [getA(Board,X,Y) ! {next} || Y <- lists:seq(1,Height-2) ] || X <- lists:seq(1,Width-2)].
+	spawn(?MODULE,next,[Board]).
 
-createColumn(_,0) -> [];
-createColumn(X,Height) -> [create_cell(X,Height) | createColumn(X,Height-1)].
+
+next(Board)->
+	receive 
+		after 100 -> nextIteration(Board)
+	end,
+	frame ! {draw},
+	next(Board).
+
+sendNext(Pid) -> Pid ! {next}.
+
+nextIteration(Board) ->
+	[ [ sendNext(getA(Board,X,Y)) || Y <- lists:seq(1,Board#board.height-2) ] || X <- lists:seq(1,Board#board.width-2)].
 
 createBoard(Width,Height)->
-	#board{area = createBoard(Width,Height),width = Width, height = height}.
+	#board{area = createArea(Width,Height),width = Width, height = Height}.
 
 createArea(Width,Height) when Height < 3 orelse Width < 3 -> throw('invalid size');
 createArea(Width,Height) -> 
@@ -54,45 +59,46 @@ createArea(Width,Height) ->
 
 drawBoard(Board)->
 	New = [array:to_list(L) || L <- array:to_list(Board) ],
-	lists:foreach(fun(A) -> io:fwrite("~p~n",[A]) end,New).
+	lists:foreach(fun(A) -> io:format("~p~n",[A]) end,New).
 
 
 initBoard(Board) -> 
 	[[ 
 		getA(Board,X,Y) ! {init,
-			[getFromCenter(Board,X-1,Y-1),
-			getFromCenter(Board,X-1,Y),
-			getFromCenter(Board,X-1,Y+1),
-			getFromCenter(Board,X,Y-1),
-			getFromCenter(Board,X,Y+1),
-			getFromCenter(Board,X+1,Y-1),
-			getFromCenter(Board,X+1,Y),
-			getFromCenter(Board,X+1,Y+1),last
+			[getA(Board,X-1,Y-1),
+			getA(Board,X-1,Y),
+			getA(Board,X-1,Y+1),
+			getA(Board,X,Y-1),
+			getA(Board,X,Y+1),
+			getA(Board,X+1,Y-1),
+			getA(Board,X+1,Y),
+			getA(Board,X+1,Y+1)
 			]} || 
-			Y <- lists:seq(0,Board#board.height-1) ] || X <- lists:seq(0,Board#board.width-1)].
+			Y <- lists:seq(1,Board#board.height-2) ] || X <- lists:seq(1,Board#board.width-2)].
 
 
 create_cell(X,Y) ->
-	spawn(?MODULE,cell,[X,Y,dead,[],0,0]).
+	spawn_link(?MODULE,cell,[X,Y,dead,[],0,0]).
 
 cell(X,Y,State,Neighbors,Living,Received) ->
 	receive
 		{init,NewN} -> cell(X,Y,State,NewN,0,0);
 		
 		{state,dead} -> case Received =:= length(Neighbors) of		
-							true -> %io:fwrite("dead ~p ~p ~p ~n",[X,Y,Neighbors]),
-							cell(X,Y,determineState(State,Living),Neighbors,0,0);
+							true ->  cell(X,Y,determineState(State,Living),Neighbors,0,0);
 							false -> cell(X,Y,State,Neighbors,Living,Received + 1)
 						end;
 		
 		{state,alive} -> case Received =:= length(Neighbors) of
-							true -> cell(X,Y,determineState(State,Living + 1),Neighbors,0,0);
-							false -> io:fwrite("alive ~p ~p ~n",[Received,Living]),
-							cell(X,Y,State,Neighbors,Living + 1,Received + 1)
+							true ->  cell(X,Y,determineState(State,Living + 1),Neighbors,0,0);
+							false -> cell(X,Y,State,Neighbors,Living + 1,Received + 1)
 						end;
+		{debug} -> io:format("~p ~p ~p ~p ~p ~p~n",[X,Y,State,Neighbors,Living,Received]),
+		cell(X,Y,State,Neighbors,Living,Received); 
+
 		{next} -> 
 					sendState(Neighbors,State),
- 					io:fwrite("~p ~p ~n",[State,Neighbors]),
+					frame ! {update,X,Y,State},
 					cell(X,Y,State,Neighbors,Living,Received)
 	end.
  
@@ -108,5 +114,7 @@ determineState(dead,Living) ->
 		false -> dead
 	end. 
 
-sendState(last,_) -> last;
-sendState([First|Rest],State) -> First ! {state,State},sendState(Rest,State).
+sendState([],_) -> ok;
+sendState([A],State) -> A ! {state,State};
+sendState([First|Rest],State) -> First ! {state,State}, sendState(Rest,State).
+
--- view.erl
+++ view.erl
@@ -7,21 +7,28 @@ initFrame(Width,Height) ->
 	 array:new( [{size, Width}, {default, array:new( [{size, Height},{default,o}] )}] ).
 
 start(Width,Height)->
-	register(frame,spawn(?MODULE,initFrame,[Width,Height])).
+	spawn(?MODULE,initFrame,[Width,Height]).
 
-set(X,Y,Value,Frame) ->
-	array:set(Y,Value,array:get(X,Frame)).
+%set(X,Y,Value,Frame) ->
+%	array:set(Y,Value,array:get(X,Frame)).
+drawBoard(Board)->
+	New = [array:to_list(L) || L <- array:to_list(Board) ],
+	lists:foreach(fun(A) -> io:format("~p~n",[A]) end,New).
 
+set( X, Y, Value, Array ) ->
+	Y_array = array:get( X, Array ),
+	New_y_array = array:set( Y, Value, Y_array ),
+	array:set( X, New_y_array, Array ).
 
 loop(Frame,Width,Height,Received) when Width*Height == Received ->
-	New = [array:to_list(L) || L <- array:to_list(Frame) ],
-	lists:foreach(fun(A) -> io:fwrite("~p~n",[A]) end,New),
+	drawBoard(Frame),
 	loop(Frame,Width,Height,0);
 
 loop(Frame,Width,Height,Received) ->
 	receive 
 	{update,X,Y,State} -> case State of
 								alive -> NewFrame = set(X,Y,x,Frame), loop(NewFrame,Width,Height,Received); 
-								dead  -> NewFrame = set(X,Y,x,Frame), loop(NewFrame,Width,Height,Received) 
-						end
+								dead  -> NewFrame = set(X,Y,o,Frame), loop(NewFrame,Width,Height,Received) 
+						end;
+	{draw} 				-> drawBoard(Frame), loop(Frame,Width,Height,Received) 
 	end.
